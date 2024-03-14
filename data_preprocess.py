638537262c92a5ea9a19c3d294fb1afcbac93a59
sarvesh-krishan <skrishan@ucdavis.edu>
2024-03-13 21:08:01 -0700

    Miscellaneous visual improvements
    Fixes were advised by Prof. Liu in his 3/13 OH

--- data_preprocess.py
+++ data_preprocess.py
@@ -6,6 +6,7 @@ import seaborn as sns
 if 1:
     print("Loading data...")
     data = pd.read_csv('hpv_past_results.csv')
+    data['income_group'] = data['income_group'].str.replace('income', '').str.strip()
     print(data.head())
 
     data_2020 = data[data['year'] == 2020]
@@ -16,12 +17,13 @@ if 1:
     print(coverage_data_2020)
 
     # Create the bar plot using seaborn
-    sns.barplot(data=coverage_data_2020, x='income_group', y='coverage', color='#03989f')
+    ax = sns.barplot(data=coverage_data_2020, x='income_group', y='coverage', color='#03989f')
 
     # Set the labels and title of the plot
     plt.xlabel('Income Group')
     plt.ylabel('Mean HPV Vaccine Coverage')
     plt.title('Mean HPV Vaccine Coverage by Income Group in 2020')
+    ax.bar_label(ax.containers[0])
 
     # Display the plot
     plt.show()
