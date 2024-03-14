a65382c72d54ba7c6e40953cb4ddbedca59e3e05
sarvesh-krishan <skrishan@ucdavis.edu>
2024-03-14 01:47:24 -0700

    Used leaflet.js to add a blank world map

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
-// Select the container element
-var container = d3.select("#visualization");
+// Initialize the map
+var map = L.map('map').setView([0, 0], 2);
 
-// Define the data for the bar chart
-var data = [10, 20, 30, 40, 50];
+// Add the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18
+}).addTo(map);
 
-// Create an SVG element within the container
-var svg = container.append("svg")
-    .attr("width", 400)
-    .attr("height", 200);
+// Define the data for the circle overlay
+var data = [
+    { lat: 37.7749, lng: -122.4194 }, // San Francisco, CA
+    { lat: 51.5074, lng: -0.1278 },   // London, UK
+    { lat: 35.6895, lng: 139.6917 }    // Tokyo, Japan
+];
 
-// Create the bars based on the data
-svg.selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", function(d, i) {
-        return i * 40;
-    })
-    .attr("y", function(d) {
-        return 200 - d;
-    })
-    .attr("width", 30)
-    .attr("height", function(d) {
-        return d;
-    })
-    .attr("fill", "steelblue");
+// Loop through the data and add circles to the map
+data.forEach(function(d) {
+    L.circleMarker([d.lat, d.lng], { radius: 5, color: 'red', fillOpacity: 1 }).addTo(map);
+});
